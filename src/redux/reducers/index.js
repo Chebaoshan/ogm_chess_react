dfe6c98bbbe6210d5e634a49832e32b1652c44f1
Chebaoshan <110754799+Chebaoshan@users.noreply.github.com>
2022-10-08 23:33:43 +0900

    Delete src directory

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { reducerGame } from "./reducerGame";
-import { reducerLogin } from "./reducerLogin";
-const reducers = combineReducers({
-  reducerGame,
-  reducerLogin,
-});
-
-export default reducers;
